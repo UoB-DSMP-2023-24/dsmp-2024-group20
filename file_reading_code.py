b61292828c8abf8bf00d26c21082c058425bd32a
JieliH <jielihe601@gmail.com>
2024-03-01 15:47:48 +0000

    updata

--- file_reading_code.py
+++ file_reading_code.py
@@ -6,12 +6,14 @@ Spyder Editor
 """
 
 import pandas as pd
+import json
+config = json.load(open("config.json"))
 #%%
 lob = []
-with open("C:/Users/Vishal Chavan/Desktop/Uni/subjects/group project/UoB_Set01_2025-01-02LOBs.txt", 'r') as file:
+with open(config['path']['LOB_test1'], 'r') as file:
     for i, line in enumerate(file):
         lob.append(list(line.split('[[')))
-        if i == 30000:
+        if i == 73500:
             break
 #%%
 import ast
@@ -30,6 +32,10 @@ for line in lob:
             m_price.append(bids[len(bids)-1][0][0])
     except Exception as e:
         print(f"Error processing line: {line}. Error: {e}")
+        bids.append([[None]])
+        time.append(line[0].split(',')[0][1:])
+        n.append(0)
+        m_price.append(None)
 bidsdf = pd.DataFrame({'time':time,
                        "bid_price":[i[0:number_of_prices] for i in bids],
                        "n_bid_prices":n,
@@ -49,6 +55,10 @@ for line in lob:
             m_price.append(asks[len(asks)-1][0][0])
     except Exception as e:
         print(f"Error processing line: {line}. Error: {e}")
+        bids.append([[None]])
+        time.append(line[0].split(',')[0][1:])
+        n.append(0)
+        m_price.append(None)
 asksdf = pd.DataFrame({'time':time,
                        "ask_price":[i[0:number_of_prices] for i in asks],
                        "n_ask_price":n,
@@ -57,8 +67,8 @@ asksdf = pd.DataFrame({'time':time,
 df = pd.merge(bidsdf,asksdf,on='time',how='outer').dropna()
 df['market_price'] = (df['max_bid']+df['min_ask'])/2
 #%%
-tapes = pd.read_csv("C:/Users/Vishal Chavan/Desktop/Uni/subjects/group project/UoB_Set01_2025-01-02tapes.csv",
+tapes = pd.read_csv(config['path']['Tapes_test1'],
                     nrows=999)
 #%%
-df[['max_bid','min_ask']].to_csv("maxmin.csv")
+#df[['max_bid','min_ask']].to_csv("maxmin.csv")
 #%%
