da09b4944e72280256870459d4eee1efd6b60eb7
fanxinweizuiniubi <fanxinwei76@gmail.com>
2024-04-21 17:14:28 +0100

    mod: arima

--- model/arima/arima_aws.py
+++ model/arima/arima_aws.py
@@ -1,9 +1,5 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pylab as plt
 from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-import pmdarima as pm
 
 stock_file = '/Users/fanxinwei/Desktop/code/git_repo/dsmp-2024-group20/input_data/total_lob_30.csv'
 df = pd.read_csv(stock_file)
@@ -16,25 +12,14 @@ df.set_index('datetime', inplace=True)
 
 price = df['avg_price']
 
-train_data = price[:-10]
-test_data = price[-10:]
+train_data = price[:-100]
+test_data = price[-100:]
 train_data_diff = train_data.diff(1)
 train_data_diff = train_data_diff.dropna()
 
-
-## 自动寻找最佳ARIMA模型
-### remember to check y is base on with training data: train_avg ? or train_wavg?
-auto_model = pm.auto_arima( train_data, start_p=0, start_q=0,
-                            test='adf',
-                            max_p=10, max_q=10,
-                            seasonal=False,   ## if seasonal=True, need to add "start_P = " and "D="
-                            # although "False" is been choosen, must set the "m="( but "m=" does't work)
-                            suppress_warnings= 'True',
-                            information_criterion= 'aic', ## using aic to choose the best model
-                            error_action='ignore',
-                            stepwise= False,    ## “True”(default) may cannot find the best way, but "False" will waste more time.
-                            trace= True   ## print the detail of the searching process
-                            )
-auto_model.fit
-
-print(auto_model.summary())
+model = ARIMA(train_data, order=(2, 1, 2))
+results = model.fit()
+forecast = results.forecast(steps=100)
+forecast = results.forecast(steps=100)
+forecast_df = pd.DataFrame(forecast.values, index=test_data.index)
+forecast_df.to_csv('forecast_arima.csv')
